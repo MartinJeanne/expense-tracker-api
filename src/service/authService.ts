46466a573164eae928b83dcb0d9c7246c15ae27a
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-04 21:34:16 +0200

    register finished

--- src/service/authService.ts
+++ src/service/authService.ts
@@ -5,29 +5,38 @@ import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 
 export async function register(req: Request, res: Response) {
-    res.send('');
+    const body = req.body;
+    if (!body) return res.send('No body');
+    else if (!User.isRaw(body)) return res.send('Body is not User');
+
+    const userRepo = await AppDataSource.getRepository(User);
+    const foundUser = await userRepo.findOneBy({ username: body.username });
+    if (foundUser) {
+        return res.send('A user with this username albready exist');
+    }
+
+    const saltRounds = 8;
+    const hashedPassword = await bcrypt.hash(body.password, saltRounds);
+    const savedUser = await userRepo.save(new User(body.username, hashedPassword));
+    res.send(savedUser);
 }
 
 export async function login(req: Request, res: Response) {
     const body = req.body;
     if (!body) return res.send('No body');
-    else if (!User.isRaw(body)) return res.send('Body not expense');
+    else if (!User.isRaw(body)) return res.send('Body is not User');
+
     const userRepo = await AppDataSource.getRepository(User);
+    const foundUser = await userRepo.findOneBy({ username: body.username });
+    if (!foundUser) {
+        return res.send('Name of user is not correct');
+    }
 
-    try {
-        const foundUser = await userRepo.findOneBy({ username: body.username });
-        if (!foundUser) {
-            return res.send('Name of user is not correct');
-        }
-
-        const isMatch = bcrypt.compareSync(body.password, foundUser.password);
-        if (isMatch) {
-            const token = jwt.sign({ id: foundUser.id.toString(), username: foundUser.username }, 'salut1', { expiresIn: '2 days', });
-            res.send({ username: foundUser.username, token: token });
-        } else {
-            return res.send('Password is not correct');
-        }
-    } catch (error) {
-        throw error;
+    const isMatch = bcrypt.compareSync(body.password, foundUser.password);
+    if (isMatch) {
+        const token = jwt.sign({ id: foundUser.id.toString(), username: foundUser.username }, 'salut1', { expiresIn: '2 days', });
+        res.send({ username: foundUser.username, token: token });
+    } else {
+        return res.send('Password is not correct');
     }
 }
