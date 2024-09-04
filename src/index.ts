bd79f4cee8470e83cfb99a694b27959291a56908
MartinJeanne <martinjeanne.dev@gmail.com>
2024-09-04 21:51:47 +0200

    added auth middleware with JWT

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,12 @@ import express from 'express';
 import { AppDataSource } from './AppDataSource';
 import expenseController from './controller/expenseController';
 import authController from './controller/authController';
+import { authMiddleware } from './middleware/authMiddleware';
 
 const app = express();
 const port: number = 3000;
 
-app.use('/api/expenses', expenseController);
+app.use('/api/expenses', authMiddleware, expenseController);
 app.use('/api/auth', authController);
 
 app.listen(port, async () => {
---
+++ src/middleware/authMiddleware.ts
@@ -0,0 +1,31 @@
+import jwt, { Secret, JwtPayload } from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+export const SECRET_KEY: Secret = 'salut1';
+
+export interface CustomRequest extends Request {
+    user: string | JwtPayload;
+}
+
+export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    try {
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) {
+            return res.status(401).json({ message: 'No token provided' });
+        }
+
+        jwt.verify(token, SECRET_KEY, (err, user) => {
+            if (err) {
+                return res.status(401).json({ message: 'Invalid token' });
+            } else if (!user) {
+                return res.status(401).json({ message: 'User not found in token' });
+            }
+            (req as CustomRequest).user = user;
+        });
+
+        next();
+    } catch (err) {
+        res.status(401).send('Please authenticate');
+    }
+};
